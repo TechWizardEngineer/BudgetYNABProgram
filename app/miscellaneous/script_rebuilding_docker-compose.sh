728a42b0db92503e072f8c00233b220334ba4ee6
kamynz16 <kamynz16@gmail.com>
2023-01-25 15:31:20 -0500

    fix: Adding comand in Dockerfile to build an image that can work better according to changes

--- app/miscellaneous/script_rebuilding_docker-compose.sh
+++ app/miscellaneous/script_rebuilding_docker-compose.sh
@@ -1,7 +1,15 @@
 #Step 1: Checking Dockerfile
 echo "Step 1: Checking Dockerfile"
 echo ""
-echo "Step 2: Checking docker-compose.yml and assign ports"
+echo "Step 2.1: Checking docker-compose.yml and assign ports"
+echo ""
+echo "Step 2.2: Run script_clean_docker_space to rebuild it"
 echo ""
 echo "Step3: Run docker-compose build, if there are major changes in app"
 docker-compose build
+echo ""
+echo "Step4: Check result with docker-compose up"
+docker-compose up
+echo ""
+echo "Step5: If this docker that has been rebuild works, it is the one that can use with the Docker App "
+
