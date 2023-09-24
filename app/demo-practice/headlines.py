1d79258b0bacae60146818af00553f459bca9112
Camila Martinez <kamynz16@gmail.com>
2023-09-23 19:09:11 -0500

    Delete app/demo-practice directory

--- app/demo-practice/headlines.py
+++
@@ -1,9 +0,0 @@
-def headline(text:str, align: bool = True) -> str:
-  if align:
-    return f"{text.title()}\n{'-' * len(text)}"
-  else:
-    return f"{text.title()} ".center(50,"o")
-
-print(headline("python type checking")) # when is with the default align=True
-
-print(headline("python type checking",align="center")) # when is with the default align=False
